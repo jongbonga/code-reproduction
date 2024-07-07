f4279ba13ce0388c43d443f711653be9edf2b618
Jonathan Ngbonga <jongbonga@gmail.com>
2024-07-07 16:32:07 +0300

    capacitor 6

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import axios from 'axios';
+import { CapacitorHttp } from '@capacitor/core';
 
 @Component({
   selector: 'app-home',
@@ -7,16 +7,45 @@ import axios from 'axios';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  selectedFile: File | null = null;
+
   constructor() {}
 
-  request() {
-    axios
-      .get('https://api.github.com/search/users?q=ionic-team')
-      .then((res) => {
-        console.log(res);
+  onFileSelected(event: any): void {
+    this.selectedFile = event.target.files[0] || null;
+  }
+
+  uploadImage(withContent = false): void {
+    if (!this.selectedFile) {
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append('name', 'avatar');
+    formData.append('id', '5');
+    formData.append('file', this.selectedFile);
+
+    this.uploadToFileio(formData, withContent);
+  }
+
+  async uploadToFileio(body: FormData, withContent = false) {
+    const headers: HeadersInit = {
+      Accept: 'application/json',
+    };
+    if (withContent) {
+      headers['Content-Type'] = 'multipart/form-data';
+    }
+
+    await fetch('https://file.io', {
+      method: 'POST',
+      body,
+      headers,
+    })
+      .then(async (res) => {
+        console.log(`with content? ${withContent}`, await res?.json());
       })
       .catch((err) => {
-        console.log(err);
+        console.log(`with content? ${withContent}`, err);
       });
   }
 }
